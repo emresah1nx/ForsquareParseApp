4264c636f2e0d6b9fa4b08326f43398e2525b3f8
emresahin <aaemresahinn@gmail.com>
2024-07-01 15:41:32 +0300

    update
    -

---
+++ FoursquareParseApp/PlacesVC.swift
@@ -0,0 +1,47 @@
+//
+//  PlacesVC.swift
+//  FoursquareParseApp
+//
+//  Created by Emre Şahin on 30.06.2024.
+//
+
+import UIKit
+import Parse
+
+class PlacesVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "LogOut", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logOutButtonClicked))
+        
+    }
+    
+    
+    @objc func addButtonClicked() {
+        //segue
+        
+    }
+    
+    @objc func logOutButtonClicked() {
+        
+        PFUser.logOutInBackground { (Error) in
+            if Error != nil {
+                let alert = UIAlertController(title: "Error", message: Error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                self.performSegue(withIdentifier: "toSignUpVC", sender: nil)
+            }
+        }
+        
+    }
+
+  
+
+}
